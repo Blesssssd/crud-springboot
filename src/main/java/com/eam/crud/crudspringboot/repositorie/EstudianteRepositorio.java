5b0d6a12f5b68a4f0446d7a62a49582c8baf5da8
Blesssssd <potes.juan.0239@eam.edu.co>
2023-03-14 10:19:34 -0500

    Second commit

--- src/main/java/com/eam/crud/crudspringboot/repositorie/EstudianteRepositorio.java
+++ src/main/java/com/eam/crud/crudspringboot/repositorie/EstudianteRepositorio.java
@@ -1,9 +1,7 @@
 package com.eam.crud.crudspringboot.repositorie;
 import com.eam.crud.crudspringboot.models.Estudiante;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface EstudianteRepositorio extends JpaRepository <Estudiante,Long>{
     
 }
--- src/main/java/com/eam/crud/crudspringboot/service/EstudianteServicioImpl.java
+++ src/main/java/com/eam/crud/crudspringboot/service/EstudianteServicioImpl.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.eam.crud.crudspringboot.repositorie.EstudianteRepositorio;
+
 import com.eam.crud.crudspringboot.models.Estudiante;
+import com.eam.crud.crudspringboot.repositorie.EstudianteRepositorio;
+
 
 @Service
-
 public class EstudianteServicioImpl implements EstudianteServicio{
     
     @Autowired
